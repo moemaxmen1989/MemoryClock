8eda55f2a3f18f53d73c5798eef9f5c65cf8a793
moedeera <82620264+moedeera@users.noreply.github.com>
2021-07-27 11:17:38 -0600

    Added a little more JavaScript
    Added JavaScript to the upper buttons

--- views/main.js
+++ views/main.js
@@ -1,12 +1,28 @@
 
 
 const play = document.getElementById("playbutton");
-
+const start = document.getElementById("start");
 
 play.addEventListener('click', (e)=>{
-
 e.preventDefault();
 document.getElementById("form").classList.remove("before");
-play.innerHTML = "<a> Start </a>";
+document.getElementById("start").classList.remove("before");
+play.classList.add("before")
+})
+
+document.getElementById("main-form").addEventListener('submit', (e)=>{
+
+e.preventDefault();
+
+
+document.getElementById("pause").classList.remove("before");
+document.getElementById("form").classList.add("before");
+document.getElementById("start").classList.add("before");
 
 })
+
+document.getElementById("pause").addEventListener('click',(e)=>{
+e.preventDefault();
+
+})
+
