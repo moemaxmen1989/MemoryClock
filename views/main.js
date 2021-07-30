5dc9ed920dd1f92a88d3e96a54f42dc2d4b54022
moedeera <82620264+moedeera@users.noreply.github.com>
2021-07-30 13:07:50 -0600

    Minor changes
    Just completed the hamberger menu

--- views/main.js
+++ views/main.js
@@ -11,3 +11,12 @@ document.getElementById("btn2").addEventListener("click", (e) => {
     document.getElementById("btn2").classList.add("before")
     document.getElementById("input1").classList.add("before")
 })
+// hamburger menu
+function myFunction() {
+    var x = document.getElementById("myLinks")
+    if (x.style.display === "block") {
+        x.style.display = "none"
+    } else {
+        x.style.display = "block"
+    }
+}
