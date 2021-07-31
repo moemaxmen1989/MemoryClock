693857ab3bb8cd341200f51f2c4640f3751adc3e
Rohan Tejeswar <roh123098@gmail.com>
2021-07-31 14:24:55 +0530

    game logic implimented - WIP

--- views/game.js
+++ views/game.js
@@ -1,16 +1,55 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max)
+/**
+ * Game defines and stores the entire game
+ *
+ * 0 = Welcome state
+ * 1 = Player has entered their name
+ * 2 = Game has begin
+ * 3 = Game has ended
+ *
+ * @param advanceGameState  This advances the game state to the next state
+ * @param getGameState      This returns the value of the current game state
+ * @param setScore          This sets the score for each level of the game
+ * @param reset             This resets the game to start a fresh one
+ * @param setPlayerName     This sets the name of the current player
+ */
+class Game {
+    constructor() {
+        this.gameState = 0
+        this.gameStateEvent = new Event("gameState")
+        this.gameScore = {}
+        this.playerName = null
+    }
+
+    advanceGameState() {
+        if (this.gameState !== 3) {
+            this.gameState += 1
+            document.dispatchEvent(this.gameStateEvent)
+            return true
+        }
+
+        return false
+    }
+
+    getGameState() {
+        return this.gameState
+    }
+
+    setScore(level, score) {
+        this.gameScore[level] = score
+    }
+
+    reset() {
+        this.gameScore = {}
+        this.playerName = null
+    }
+
+    setPlayerName(name) {
+        const trimmedName = name.trim()
+        if (trimmedName === "") return false
+
+        this.playerName = trimmedName
+        return true
+    }
 }
 
-const template = document.querySelector("#coloredCox")
-const container = document.querySelector("#gridContainer")
-const SIZE = 6
-
-for (let i = 0; i < SIZE; i++) {
-    console.log("i: ", i)
-    const clonedBox = template.content.cloneNode(true)
-    const clonedDiv = clonedBox.querySelector(".colored-box")
-    clonedDiv.style.backgroundColor = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`
-    console.log("clonedDiv: ", clonedDiv.style)
-    container.append(clonedBox)
-}
+const game = new Game()
--- views/main.js
+++ views/main.js
@@ -3,13 +3,24 @@ document.getElementById("btn1").addEventListener("click", (e) => {
     document.getElementById("btn1").classList.add("before")
     document.getElementById("btn2").classList.remove("before")
     document.getElementById("input1").classList.remove("before")
+
+    game.advanceGameState()
 })
 
 document.getElementById("btn2").addEventListener("click", (e) => {
     e.preventDefault()
 
-    document.getElementById("btn2").classList.add("before")
-    document.getElementById("input1").classList.add("before")
+    const button2 = document.getElementById("btn2")
+    const input1 = document.getElementById("input1")
+
+    if (game.setPlayerName(input1.value)) {
+        game.advanceGameState()
+        input1.classList.remove("error")
+        input1.classList.add("before")
+        button2.classList.add("before")
+    } else {
+        input1.classList.add("error")
+    }
 })
 // hamburger menu
 function myFunction() {
